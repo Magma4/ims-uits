dda7050cca1d824c3e2ea86f2b79d01ba586eff6
Magma4 <youngmagma@protonmail.com>
2024-02-22 15:58:34 +0000

    Creating admin dashboard

--- ims/inventory/admin.py
+++ ims/inventory/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Stock
+
+admin.site.site_header = 'UITS INVENTORY ADMINISTRATION'
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'quantity')
+    list_filter = ('name',)
 
 # Register your models here.
+admin.site.register(Stock, StockAdmin )
--- ims/inventory/models.py
+++ ims/inventory/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Stock(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
-    quantity = models.PositiveIntegerField()
+    name = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=200, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.name}-{self.quantity}'
 
