410ed91bcaf08bc336855bfed515904ffc538b31
Magma4 <youngmagma@protonmail.com>
2024-02-22 18:38:29 +0000

    Building the models

--- ims/inventory/models.py
+++ ims/inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Stock(models.Model):
@@ -8,4 +9,11 @@ class Stock(models.Model):
 
     def __str__(self):
         return f'{self.name}-{self.quantity}'
+    
+class Order(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE, null=True)
+    Users = models.ForeignKey(User, models.CASCADE, null=True )
+    order_quantity = models.PositiveIntegerField(null=True)
+    order_description = models.CharField(max_length=200, null=True)
+    date = models.DateTimeField(auto_now_add=True)
 
