ba64162205186fc33dfeb5f4ce1ec1119598df6d
Magma4 <youngmagma@protonmail.com>
2024-03-14 16:35:53 +0000

    Adding instructions to use page

--- ims/inventory/urls.py
+++ ims/inventory/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('report', views.report, name='report'),
     path('order_excel', views.order_excel, name='order_excel'),
     path('order_pdf', views.order_pdf, name='order_pdf'),
+    path()
     
 ]
