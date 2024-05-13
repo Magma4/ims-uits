d414287b28ce1e7af70a2ce4a4bd758b34c88c23
Magma4 <youngmagma@protonmail.com>
2024-05-13 17:52:42 +0000

    Celery

--- ims/ims/celery.py
+++ ims/ims/celery.py
@@ -4,21 +4,17 @@ from celery import Celery
 from django.conf import settings
 from celery.schedules import crontab
 
-# Set the default Django settings module for the 'celery' program.
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ims.settings')
 
 app = Celery('ims')
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     'send_due_reminders_every_morning': {
         'task': 'inventory.tasks.send_reminder_emails',
-        'schedule': crontab(hour=00, minute=46),  # Corrected to run every day at 8:00 AM
+        'schedule': crontab(hour=14, minute=46), 
     },
 }
